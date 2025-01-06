532781ceaa7bd9f9ed7f8948761acb7ae8043e06
Najjahi <157368284+Najjahi@users.noreply.github.com>
2025-01-06 10:10:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
+
+@app.route('/')
 def hello_world():
     return "<h2>Bonjour tout le monde ! </h2> <p> Pour accéder à vos exercices cliquer <a href='./exercices/'Ici</a></p>"
 
 
-@app.route('/')
+
 def hello_world():
     return render_template('index.html')
 
